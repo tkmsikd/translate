cc0bc0781826e601c0559dba3cfa72379a1200bb
Takamasa Ikeda <takamasa.ikeda@toyota.com>
2022-04-03 21:58:56 -0500

    refactored service data

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,92 @@
+use curl::easy::{Easy2, Handler, WriteError};
 use std::env;
-use std::fs::File;
-use std::{fmt::Debug, io::BufReader};
-use serde::de::value::Error;
-use serde_json;
-use serde::Deserialize;
+use std::{fmt::Debug};
+use std::fmt;
+
+pub mod service_handler;
+use service_handler::{ServiceData, ServiceType};
 
 
-#[allow(unused)]
-#[derive(Debug, PartialEq, Deserialize)]
-struct ServiceData {
-    url: String,
-    authentication_key: String,
+// TODO:
+/// - まずはシンプルに動かすMock
+/// - 非同期にリファクタ
+/// - サービスごとにtrait, 実装ファイルを分割
+/// - オブジェクト指向にリファクタ
+/// - セキュアバイ・デザイン: ドメインプリミティブ型に変更
+///
+
+#[derive(Debug, Clone, Copy)]
+enum TargetLanguage {
+    JP,
+    EN,
+    Undefined
 }
 
-#[derive(Debug, PartialEq)]
-enum ServiceType {
-    Deepl,
-    GoogleTranslate,
+impl fmt::Display for TargetLanguage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TargetLanguage::JP => write!(f, "JP"),
+            TargetLanguage::EN => write!(f, "EN"),
+            TargetLanguage::Undefined => write!(f, "undefined")
+        }
+    }
+}
+
+#[derive(Debug)]
+struct HttpRequest{
+    pub serviceData: ServiceData,
+    pub text: String,
+    pub target_lang: TargetLanguage,
+}
+
+impl HttpRequest {
+    fn generate_request(&self)  -> String{
+        let url = &self.serviceData.url;
+        let auth_key = &self.serviceData.authentication_key;
+        let text = &self.text;
+        let target_lang = self.target_lang.to_string();
+
+        let full = format!("{}?{}?{}?{}", url, auth_key, text, target_lang);
+        full.to_string()
+    }
+}
+
+struct Collector(Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0.extend_from_slice(data);
+        Ok(data.len())
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let _service_type = load_service_type(&args);
 
-    println!("{:?}", _service_type);
-    let _service_data = load_service_data(_service_type).unwrap();
+    let _service_type = ServiceType::load_service_type(&args[0]);
 
-    println!("{:?}",_service_data.url);
+    let sd = ServiceData{url: "".to_string(), authentication_key: "".to_string()};
+    let service_data:ServiceData = ServiceData::new(&sd , _service_type);
 
 
-}
 
-fn load_service_type(args: &Vec<String>) -> ServiceType {
-    let service = args[0].as_str();
-    match service {
-        "google" => ServiceType::GoogleTranslate,
-        "deepl" | _ => ServiceType::Deepl,
-    }
-}
-
-fn load_service_data(service_type: ServiceType) -> Result<ServiceData, Box<Error>> {
-    let credential_path = match service_type {
-        ServiceType::Deepl => "./src/deepl_credential.json",
-        ServiceType::GoogleTranslate => "./src/google_translate_credential.json",
+    let tmp_request = HttpRequest{
+        serviceData: service_data,
+        text: "Hello Wold".to_string(),
+        target_lang: TargetLanguage::JP
     };
 
-    let file = File::open(credential_path).expect("file not found");
-    let reader = BufReader::new(file);
-    let deserialize = serde_json::from_reader(reader).unwrap();
+    println!("{:?}", tmp_request);
 
-    Ok(deserialize)
+    //let mut easy = Easy2::new(Collector(Vec::new()));
+    //easy.post(true).unwrap();
+    //easy.url().unwrap();
+
+    //assert_eq!(easy.response_code().unwrap(), 200);
+    //let contents = easy.get_ref();
+    //println!("{}", String::from_utf8_lossy(&contents.0));;
 }
-
-
+// Test 
 #[cfg(test)]
 mod tests {
     use std::vec;
@@ -61,20 +95,20 @@ mod tests {
 
     #[test]
     fn test_load_service_type() {
-        let service_type: ServiceType = ServiceType::Deepl;
         let args_vec = vec!["deepl".to_string()];
-        assert_eq!(service_type, load_service_type(&args_vec));
+        assert_eq!(ServiceType::Deepl, ServiceType::load_service_type(&args_vec[0]));
     }
 
     #[test]
     fn test_load_service_data() {
-        let placeholder_data: ServiceData = ServiceData {
-            url: "placeholder".to_string(),
-            authentication_key: "placeholder".to_string(),
+        let _service_type = ServiceType::Deepl;
+        let sd = ServiceData{url: "".to_string(), authentication_key: "".to_string()};
+        let service_data:ServiceData = ServiceData::new(&sd , _service_type);
+        let correct_data = ServiceData {
+            url:"https://api-free.deepl.com/v2/translate".to_string(),
+            authentication_key: "819ee1f7-f31b-1bc8-03a6-5e5a33b7bbd5:fx".to_string(),
         };
-
-        let service_type = ServiceType::Deepl;
-
-        assert_eq!(placeholder_data, load_service_data(service_type).unwrap());
+        assert_eq!(correct_data, service_data);
+    
     }
 }
---
+++ src/service_handler.rs
@@ -0,0 +1,55 @@
+use serde::de::value::Error;
+use serde::Deserialize;
+use serde_json;
+use std::io::BufReader;
+use std::fs::File;
+
+
+#[allow(unused)]
+
+#[derive(Debug, PartialEq)]
+pub enum ServiceType {
+    Deepl,
+    GoogleTranslate,
+}
+
+impl ServiceType {
+    pub fn load_service_type(service: &str) -> Self {
+        match service {
+            "google" => Self::GoogleTranslate,
+            "deepl" | _ => Self::Deepl,
+        }
+    }
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct ServiceData {
+    pub url: String,
+    pub authentication_key: String,
+}
+
+impl ServiceData {
+    pub fn new(&self, service_type: ServiceType) -> Self {
+        let (url, auth_key) = self.load_service_data(service_type).unwrap();
+
+        Self { 
+            url: url,
+            authentication_key: auth_key
+        }
+    }
+
+    fn load_service_data(&self, service_type: ServiceType) -> Result<(String, String), Box<Error>> {
+        let credential_path = match service_type {
+            ServiceType::Deepl => format!("./auth_files/deepl_auth_info.json"),
+            ServiceType::GoogleTranslate => format!("./auth_files/gtranslate_auth_info.json"),
+        };
+    
+        let file = File::open(credential_path).expect("file not found");
+        let reader = BufReader::new(file);
+        let deserialize:ServiceData = serde_json::from_reader(reader).unwrap();
+
+        let url = deserialize.url;
+        let auth_key = deserialize.authentication_key;
+    
+        Ok((url, auth_key))
+    } 
+}
